e392e4d3eeded5cdb07c147b830f90f8d9fe8dfd
yaghob abdi <y.abdi@rashasoft.ir>
2024-03-19 08:37:10 +0330

    Test

--- src/test/java/ir/msob/jima/test/SampleClassTest.java
+++ src/test/java/ir/msob/jima/test/SampleClassTest.java
@@ -1,9 +1,11 @@
 package ir.msob.jima.test;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class SampleClass {
     @Test
     void test() {
+        assertNotNull(null);
     }
 }
