3fc5255ce65971e7a776b610cb5d96478fa6367c
Yaqub Abdi <88418835+yaqub-abdi@users.noreply.github.com>
2024-03-31 13:35:43 +0330

    Update ng-publish.sh

--- web/angular/ng-publish.sh
+++ web/angular/ng-publish.sh
@@ -2,13 +2,14 @@ npm install
 for project in $(ls projects/); do
   cd projects/$project
   npm install
-  npm update $PUBLISH_REGISTRY
+  npm update
 #  npm unpublish @{ORG}/$project $PUBLISH_REGISTRY -f
   cd ../..
   npm test --project=$project
   ng build --project=$project
   cd dist/$project
-  npm publish $PUBLISH_REGISTRY
+  #npm publish $PUBLISH_REGISTRY
+  npm publish
   cd ../..
 done
 
