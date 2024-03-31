76dbd67ec3756151c59e7463e1384ccc857c1771
yaghob abdi <y.abdi@rashasoft.ir>
2024-03-31 14:38:19 +0330

    success test pr

--- web/angular/ng-publish.sh
+++ web/angular/ng-publish.sh
@@ -3,11 +3,10 @@ for project in $(ls projects/); do
   cd projects/$project
   npm install
   npm update
-#  npm unpublish @{ORG}/$project $PUBLISH_REGISTRY -f
   cd ../..
   npm test --project=$project
   ng build --project=$project
-  cd dist/$project
+  cd projects/$project
   #npm publish $PUBLISH_REGISTRY
   npm publish --access public
   cd ../..
